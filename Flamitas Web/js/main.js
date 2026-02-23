8b5d8c39910cd863d900ae5341e51a85b9081031
Vicky <mavagaza@gmail.com>
2026-02-22 22:05:44 -0300

    Delete Flamitas Web directory

--- Flamitas Web/js/main.js
+++
@@ -1,6 +0,0 @@
-let menuToggle = document.querySelector('.menu-toggle');
-let menuLinks = document.querySelector('.menu-links');
-
-menuToggle.addEventListener('click', () => {
-  menuLinks.classList.toggle('active');
-});
