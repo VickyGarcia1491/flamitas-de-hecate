3d3c81372151482e033831bf6ab98721027236d1
Vicky <mavagaza@gmail.com>
2026-05-27 00:06:05 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
-let menuToggle = document.querySelector('.menu-toggle');
-let menuLinks = document.querySelector('.menu-links');
+window.addEventListener("load", inicio);
 
-menuToggle.addEventListener('click', () => {
-  menuLinks.classList.toggle('active');
-});
+function inicio() {
+
+    // BOTON MENU HAMBURGUESA
+    document.querySelector("#menuToggle").addEventListener("click", toggleMenu);
+
+    // LINKS DEL MENU
+    let links = document.querySelectorAll("#menuLinks a");
+
+    for (let i = 0; i < links.length; i++) {
+        links[i].addEventListener("click", cerrarMenu);
+    }
+}
+
+
+// ---------------- MENU ----------------
+
+function toggleMenu() {
+    let menu = document.querySelector("#menuLinks");
+
+    // Si tiene la clase active → la saco
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+    } else {
+        menu.classList.add("active");
+    }
+}
+
+
+function cerrarMenu() {
+    let menu = document.querySelector("#menuLinks");
+    menu.classList.remove("active");
+}
